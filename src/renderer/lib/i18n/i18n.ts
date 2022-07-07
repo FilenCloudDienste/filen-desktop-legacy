d2798664e0a4c44c6636a0b76d1947b09c9eff95
Beventar <51784467+Beventar@users.noreply.github.com>
2022-07-07 20:03:22 +0200

    Update i18n.ts

--- src/renderer/lib/i18n/i18n.ts
+++ src/renderer/lib/i18n/i18n.ts
@@ -2,6 +2,7 @@ import en from "./lang/en"
 import de from "./lang/de"
 import ru from "./lang/ru"
 import uk from "./lang/uk"
+import pl from "./lang/pl"
 
 const translations: {
     [key: string]: any
@@ -9,7 +10,8 @@ const translations: {
     en,
     de,
     ru,
-    uk
+    uk,
+    pl
 }
 
 export const i18n = (lang: string = "en", text: string, firstUpperCase: boolean = true, replaceFrom: string[] = [], replaceTo: string[] = []) => {
